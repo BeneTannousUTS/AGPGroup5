9edd2458b549c221569868ef4f8bb835e2901638
Zac Slater <zachary.slater@student.uts.edu.au>
2024-10-18 15:39:22 +1100

    adding pcg assessment 3

---
+++ Source/AGP/AGPGameInstance.h
@@ -0,0 +1,31 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/GameInstance.h"
+#include "AGPGameInstance.generated.h"
+
+class AWeaponPickup;
+/**
+ * 
+ */
+UCLASS()
+class AGP_API UAGPGameInstance : public UGameInstance
+{
+	GENERATED_BODY()
+
+public:
+
+	UClass* GetWeaponPickupClass() const;
+	UClass* GetMoneyPickupClass() const;
+	int32 Balance = 0;
+	void UpdateBalance(int32 Change);
+
+protected:
+
+	UPROPERTY(EditDefaultsOnly, Category="Pickup Classes")
+	TSubclassOf<AWeaponPickup> WeaponPickupClass;
+	UPROPERTY(EditDefaultsOnly, Category="Pickup Classes")
+	TSubclassOf<AWeaponPickup> MoneyPickupClass;
+	
+};
---
+++ Source/AGP/ProceduralLandscape.h
@@ -0,0 +1,88 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "ProceduralMeshComponent.h"
+#include "Pathfinding/NavigationNode.h"
+#include "ProceduralLandscape.generated.h"
+
+UCLASS()
+class AGP_API AProceduralLandscape : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AProceduralLandscape();
+	virtual bool ShouldTickIfViewportsOnly() const override;
+	void GenerateBase();
+	void GenerateTunnels();
+	void GenerateCliffs();
+	void GenerateMesh() const;
+	void GenerateSafeHouse(FVector SpawnLocation) const;
+
+protected:
+	void GenerateTerrain();
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+	UPROPERTY(VisibleAnywhere)
+	UProceduralMeshComponent* ProceduralMesh;
+
+	void ClearLandscape();
+    UPROPERTY()
+    TArray<FVector> Vertices;
+    UPROPERTY()
+    TArray<int32> Triangles;
+    UPROPERTY()
+    TArray<FVector2D> UVCoords;
+	UPROPERTY()
+	TArray<ANavigationNode*> Nodes;
+	UPROPERTY()
+	UPathfindingSubsystem* PathfindingSubsystem;
+
+	UPROPERTY(EditAnywhere)
+	bool bShouldRegenerate = false;
+	float Timer;
+	TSet<int32> HoleIndices;
+	UPROPERTY(EditAnywhere)
+	int32 TunnelLevels = 1;
+
+	UPROPERTY(EditAnywhere)
+	int32 MinWidth = 10;
+	UPROPERTY(EditAnywhere)
+	int32 MinDepth = 10;
+	UPROPERTY(EditAnywhere)
+	int32 MinHeight = 3;
+	UPROPERTY(EditAnywhere)
+	int32 MaxWidth = 10;
+	UPROPERTY(EditAnywhere)
+	int32 MaxDepth = 10;
+	UPROPERTY(EditAnywhere)
+	int32 MaxHeight = 3;
+	UPROPERTY(EditAnywhere)
+	float VertexSpacing = 1000.0f;
+
+	int32 Width;
+	int32 Depth;
+	int32 Height;
+
+	/*UPROPERTY(EditAnywhere)
+	float PerlinScale = 1000.0f;
+	UPROPERTY(EditAnywhere)
+	float PerlinRoughness = 0.00012f;
+	UPROPERTY(EditAnywhere)
+	float NoiseThreshold = 0.1f;
+	UPROPERTY(VisibleAnywhere)
+	float PerlinOffset;*/
+
+public:
+	void RemoveNavNodes();
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+private:
+	void SpawnClimbingRock(FVector SpawnLocation) const;
+
+};
